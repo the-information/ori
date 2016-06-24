1c88615687668748f6de776583378a3bfd27e085
Harry Schmidt <me@goldibex.com>
2016-06-24 14:34:01 -0400

    add kami link to doc.go

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
 /*
 Package ori provides high-level support for implementing a REST/JSON API
-on Google App Engine using Kami, including request-scoped configuration and authentication,
-JWT decoding, and middleware to handle JSON content negotiation and CORS.
+on Google App Engine using Kami (https://godoc.org/github.com/guregu/kami),
+including request-scoped configuration and authentication, JWT decoding,
+and middleware to handle JSON content negotiation and CORS.
 */
 package ori
