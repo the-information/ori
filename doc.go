9f06a7ad41ebfaf8ed42de2fbf675323c731f358
Harry Schmidt <me@goldibex.com>
2016-06-24 14:33:12 -0400

    tweak doc.go

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package ori	provides high-level support for implementing a REST/JSON API
+Package ori provides high-level support for implementing a REST/JSON API
 on Google App Engine using Kami, including request-scoped configuration and authentication,
 JWT decoding, and middleware to handle JSON content negotiation and CORS.
 */
